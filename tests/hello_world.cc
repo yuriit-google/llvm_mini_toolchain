052ff04b40aa8387c2067da92d3376fbebd216a8
Yurii Topin <yuriit@google.com>
2024-11-15 01:33:23 +0000

    Add Abseil test. Refactor another tests (C++, Protobuf)

--- tests/hello_world.cc
+++ tests/hello_world.cc
@@ -1,21 +1,10 @@
 #include <iostream>
-#include "tests/hello_world.pb.h"
 
 using namespace std;
 
 int main() {
-  helloworld::HelloRequest request;
-  request.set_name("World");
 
-  // Serialize the message to a string
-  string output;
-  request.SerializeToString(&output);
-
-  // Deserialize the message from the string
-  helloworld::HelloRequest deserialized_request;
-  deserialized_request.ParseFromString(output);
-
-  cout << "Hello, " << deserialized_request.name() << "!" << endl;
+  cout << "Hello World!" << endl;
 
   return 0;
 }
