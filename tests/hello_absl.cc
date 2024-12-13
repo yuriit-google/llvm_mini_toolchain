7d866d7dc321cf280f7a2ea31f3c0dce16031666
Yurii Topin <yuriit@google.com>
2024-12-13 15:55:49 -0800

    Add StrJoin and StrSplit tests

--- tests/hello_absl.cc
+++ tests/hello_absl.cc
@@ -3,12 +3,16 @@
 #include <vector>
 
 #include "absl/strings/str_join.h"
+#include "absl/strings/str_split.h"
+#include "absl/container/flat_hash_map.h"
 
 int main() {
-  std::vector<std::string> v = {"Hello", "Abseil"};
-  std::string s = absl::StrJoin(v, " ");
+  std::vector<std::string> v1 = absl::StrSplit("Hello,Abseil,StrSplit,and,StrSplit,methods", ',');
+  std::string s1 = absl::StrJoin(v1, " ");
+  std::cout << s1 << "!\n";
 
-  std::cout << "Joined string: " << s << "!\n";
+  absl::flat_hash_map<int, std::string> m1 = {{1, "Hello"}, {2, "flat_hash_map"}, };
+  std::cout << m1.at(1) << " " << m1.at(2) << "!\n";
 
   return 0;
 }
