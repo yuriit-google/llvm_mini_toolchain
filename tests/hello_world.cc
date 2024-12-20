91ffdfd1c8fa42b45c545b7bb9fc34a10ed6f8a2
Yurii Topin <yuriit@google.com>
2024-12-20 11:46:44 -0800

    Revert simple test. Create separate test for macos only

--- tests/hello_world.cc
+++ tests/hello_world.cc
@@ -6,21 +6,6 @@ using namespace std;
 
 int main() {
   cout << "Hello C++ World!" << endl;
-
-  std::vector<char> buffer;
-  CFTimeZoneRef tz_default = CFTimeZoneCopyDefault();
-  if (CFStringRef tz_name = CFTimeZoneGetName(tz_default)) {
-    CFStringEncoding encoding = kCFStringEncodingUTF8;
-    CFIndex length = CFStringGetLength(tz_name);
-    CFIndex max_size = CFStringGetMaximumSizeForEncoding(length, encoding) + 1;
-    buffer.resize(static_cast<size_t>(max_size));
-    if (CFStringGetCString(tz_name, &buffer[0], max_size, encoding)) {
-      //zone = &buffer[0];
-    }
-  }
-  CFRelease(tz_default);
-
-
   return 0;
 }
 
