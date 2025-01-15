cf12b6489791be2174662ad2cb7f71c1665359b2
Yurii Topin <yuriit@google.com>
2025-01-15 15:30:55 -0800

    Add license to files with source code

--- tests/protoclient_tool.py
+++ tests/protoclient_tool.py
@@ -1,16 +1,24 @@
-# Build this script by py_binary
-# Call this py_binary in genrule
+# MIT License
 #
-# proto_library(
-#     name = "hello_proto",
-#     srcs = ["hello.proto"],
-#     visibility = ["//visibility:public"],
-# )
-
+# Copyright (c) 2024 Google LLC
 #
-# Tools - for Linux (any program which run during build on host machine)
-# Target - for mac
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
 #
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 from tests import protoclient
 
